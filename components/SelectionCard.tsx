d234b26160d577a2ceeee408e6157f8df7ae9178
Marco Lemcke <marcolemcke2014@gmail.com>
2025-04-28 01:11:51 +0200

    Style: Refine SelectionCard active/inactive states

--- components/SelectionCard.tsx
+++ components/SelectionCard.tsx
@@ -22,8 +22,8 @@ const SelectionCard: React.FC<SelectionCardProps> = ({
   className = ''
 }) => {
   const baseStyle = "block w-full p-4 border rounded-lg text-left transition-colors duration-150 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-[#0A4923]";
-  const inactiveStyle = "bg-off-white/10 border-off-white/20 text-off-white/80";
-  const activeStyle = "bg-green-100 border-[#84F7AC] text-green-900";
+  const inactiveStyle = "bg-off-white/10 border-off-white/20 text-off-white/80 focus:ring-green-300";
+  const activeStyle = "bg-green-100 border-[#84F7AC] text-green-900 focus:ring-[#84F7AC]";
 
   return (
     <button
@@ -32,9 +32,9 @@ const SelectionCard: React.FC<SelectionCardProps> = ({
       className={`${baseStyle} ${selected ? activeStyle : inactiveStyle} ${className}`}
     >
       <div className="flex items-center">
-        {icon && <div className="mr-3 flex-shrink-0 ${selected ? 'text-green-700' : 'text-green-200'}">{icon}</div>}
+        {icon && <div className={`mr-3 flex-shrink-0 ${selected ? 'text-green-700' : 'text-off-white/80'}`}>{icon}</div>}
         <div className="flex-grow">
-          <h3 className={`text-base font-semibold ${selected ? 'text-green-900' : 'text-off-white'}`}>{title}</h3>
+          <h3 className={`text-base ${selected ? 'font-semibold text-green-900' : 'font-light text-off-white'}`}>{title}</h3>
           {description && (
             <p className={`text-sm mt-1 ${selected ? 'text-green-800' : 'text-off-white/80'}`}>
               {description}
